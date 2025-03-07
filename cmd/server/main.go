ce365ce21a2ffda2a5aa02126bef89635a27400e
Seb Veigli <seb.veigli@gmail.com>
2025-03-07 21:35:14 +0100

    feature: hello world

---
+++ cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+const banner = `
+ █████╗ ██╗   ██╗████████╗██╗  ██╗
+██╔══██╗██║   ██║╚══██╔══╝██║  ██║
+███████║██║   ██║   ██║   ███████║
+██╔══██║██║   ██║   ██║   ██╔══██║
+██║  ██║╚██████╔╝   ██║   ██║  ██║
+╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝`
+
+var version string // injected at build time
+
+func main() {
+	fmt.Println(banner)
+}
